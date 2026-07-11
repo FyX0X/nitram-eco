b669403babaf1a59c0539514089a6d778a6ea37a
Martin Ergo <martinergo06@gmail.com>
2026-07-12 00:29:15 +0200

    add economy system

---
+++ src/main/java/dev/nitramnibus/nitrameco/systems/EconomySystem.java
@@ -0,0 +1,54 @@
+package dev.nitramnibus.nitrameco.systems;
+
+import javax.annotation.Nonnegative;
+import java.util.HashMap;
+import java.util.UUID;
+
+
+
+// represent the money of all players, online or offline.
+// Used to not constantly query the database
+public class EconomySystem {
+
+    private static boolean instantiated = false;
+
+    private final HashMap<UUID, Long> playerMoney;
+
+
+    public EconomySystem() {
+        if (instantiated) {
+            throw new IllegalStateException("Cannot create multiple Economy System");
+        }
+        instantiated = true;
+        playerMoney = new HashMap<>();
+    }
+
+    public void setMoney(UUID uuid, @Nonnegative long money) {
+        playerMoney.put(uuid, money);
+    }
+
+    public long getMoney(UUID uuid) {
+        return playerMoney.get(uuid);
+    }
+
+    public void addMoney(UUID uuid, @Nonnegative long amount) {
+        setMoney(uuid, getMoney(uuid) + amount);
+    }
+
+    public boolean takeMoney(UUID uuid, @Nonnegative long amount) {
+        long balance = getMoney(uuid);
+        if (balance < amount) {
+            return false;
+        }
+        setMoney(uuid, balance - amount);
+        return true;
+    }
+
+    public boolean transferMoney(UUID from, UUID to, @Nonnegative long amount) {
+        if (! takeMoney(from, amount)) {
+            return false;
+        }
+        addMoney(to, amount);
+        return true;
+    }
+}
