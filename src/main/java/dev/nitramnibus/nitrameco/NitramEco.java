1bc36fc590e5f793aa51815c58469ccbb7201ff3
Martin Ergo <martinergo06@gmail.com>
2026-07-11 15:38:22 +0200

    initial commit

---
+++ src/main/java/dev/nitramnibus/nitrameco/NitramEco.java
@@ -0,0 +1,16 @@
+package dev.nitramnibus.nitrameco;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NitramEco extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
